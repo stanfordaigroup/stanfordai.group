6eb60fc55540a5609a59cac7cb15f745d8181fb6
Andrew Lee <andrewsoohwanlee@gmail.com>
2017-09-30 12:20:42 -0700

    Better 404 page.

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,13 @@
 import  * as React from 'react'
 
+import './404.scss';
+
 const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
+  <div className="pageNotFound__container">
+    <h2 className="pageNotFound__subtitle">404</h2>
+    <h2 className="pageNotFound__title">Page Not Found</h2>
     <p>You just hit a route that doesn&#39;t exist.</p>
+    <p>🚧</p>
   </div>
 )
 
