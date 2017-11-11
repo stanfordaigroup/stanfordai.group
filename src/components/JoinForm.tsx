15007e8af7f6e730592b21f881256d3b33aa8447
Andrew Lee <andrewsoohwanlee@gmail.com>
2017-11-11 14:37:31 -0800

    More testing

--- src/components/JoinForm.tsx
+++ src/components/JoinForm.tsx
@@ -1,127 +1,101 @@
 import * as React from 'react';
-import Link from 'gatsby-link';
-import {Formik, Field} from 'formik';
+import Link, {navigateTo} from 'gatsby-link';
 
 import './JoinForm.scss';
 
 function encode(data: any) {
   return Object.keys(data)
-      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-      .join("&");
+      .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+      .join('&');
 }
 
-class JoinForm extends React.Component{
+type State = {
+  fullname: string,
+  email: string,
+  year: string,
+};
+
+class JoinForm extends React.Component<null, State>{
+  state = {
+    fullname: '',
+    email: '',
+    year: '',
+  };
+
+  handleChange = (e) => {
+    this.setState({[e.target.name]: e.target.value});
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    const request: any = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: encode(
+        Object.assign({
+          'form-name': 'join',
+        }, this.state),
+      ),
+    };
+
+    fetch('/', request)
+      .then(() => alert('Success!'))
+      .catch(error => alert(error));
+
+    navigateTo('/welcome');
+  };
+
   render () {
     return (
       <div className="page">
         <div className="join__container">
-          <Formik
-            initialValues={{
-              fullname: '',
-              email: '',
-              year: '',
-            }}
-            onSubmit={(values, { setSubmitting,  setErrors, setStatus }) => {
-              // Create request to subscribe to mailman
-              // Mimicking: https://mailman.stanford.
-
-              const mailPayload = JSON.stringify({
-                'fullname': values.fullname,
-                'email': values.email,
-              });
-
-              const mailRequest: any = {
-                method: 'POST',
-                headers: {
-                  'Origin': 'https://mailman.stanford.edu',
-                  contentType: 'application/json',                
-                },
-                body: mailPayload,
-              };
-
-              const mailSubscription = fetch('https://saig-email-subscriber.herokuapp.com/subscribe', mailRequest)
-                .then((res: any) => {})
-                .catch(error => console.error(error));
-
-              const netlifyRequest: any = {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-                body: encode(Object.assign({
-                  'form-name': 'join',
-                }, values)),
-              };
-
-              console.log(netlifyRequest);
-
-              const netlifyLogging = fetch("/", netlifyRequest)
-                .then(() => {})
-                .catch(error => console.error(error));
-
-              Promise
-                .all([mailSubscription, netlifyLogging])
-                .then(() => {
-                  setSubmitting(false);
-                  setStatus('submitted');
-                });
-            }}
-            render={({ status, values, errors, isSubmitting, handleBlur, handleChange, handleSubmit }) => {
-              if (status === 'submitted') {
-                return (
-                  <div className="form__submitted">
-                    <h1>Thanks for joining SAIG, {values.fullname}!</h1>
-                    <p>Expect an email confirmation shortly asking you to join our mailing list. 😉</p>
-                  </div>
-                );
-              } else {
-                return (
-                  <form
-                    className="form"
-                    name="join"
-                    onSubmit={handleSubmit}
-                  >
-                    <div className="form__block">
-                      <label htmlFor="fullname">
-                        <h3>Name <small>* Required</small></h3>
-                      </label>
-                      <Field id="fullname" type="text" name="fullname" required placeholder="e.g., Machiney McLearnFace" />
-                    </div>
-                    <div className="form__block">
-                      <label htmlFor="email">
-                        <h3>Email <small>* Required</small></h3>
-                        <p>We'll invite you to our email list for news!</p>
-                      </label>
-                      <Field id="email" type="email" name="email" required placeholder="e.g., firstlast@stanford.edu" />
-                    </div>
-                    <div className="form__block">
-                      <label htmlFor="year">
-                        <h3>Class Year <small>* Required</small></h3>
-                      </label>
-                      <select
-                        id="year"
-                        className="form__year"
-                        name="year"
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        value={values.year}
-                        required
-                      >
-                        <option disabled value=""> -- select an option -- </option>
-                        <option value="freshman">Freshman</option>
-                        <option value="sophomore">Sophomore</option>
-                        <option value="junior">Junior</option>
-                        <option value="senior">Senior</option>
-                        <option value="other">Other (Grad, PhD, etc.)</option>
-                      </select>
-                    </div>
-                    <button className="form__submit" type="submit" disabled={isSubmitting}>Submit</button>
-                    <p className="form_subtext">
-                      <small>By submitting this form, you'll automatically be added to our mailing list.</small>
-                    </p>
-                  </form>
-                );
-              }
-            }}
-          />
+          <form
+            className="form"
+            name="join"
+            action="/welcome"
+            onSubmit={this.handleSubmit}
+          >
+            <div className="form__block">
+              <label htmlFor="fullname">
+                <h3>Name <small>* Required</small></h3>
+              </label>
+              <input id="fullname" type="text" onChange={this.handleChange} name="fullname" required placeholder="e.g., Machiney McLearnFace" />
+            </div>
+            <div className="form__block">
+              <label htmlFor="email">
+                <h3>Email <small>* Required</small></h3>
+                <p>We'll invite you to our email list for news!</p>
+              </label>
+              <input id="email" type="email" onChange={this.handleChange} name="email" required placeholder="e.g., firstlast@stanford.edu" />
+            </div>
+            <div className="form__block">
+              <label htmlFor="year">
+                <h3>Class Year <small>* Required</small></h3>
+              </label>
+              <select
+                id="year"
+                className="form__year"
+                name="year"
+                onChange={this.handleChange}
+                value={this.state.year}
+                required
+              >
+                <option disabled value=""> -- select an option -- </option>
+                <option value="freshman">Freshman</option>
+                <option value="sophomore">Sophomore</option>
+                <option value="junior">Junior</option>
+                <option value="senior">Senior</option>
+                <option value="other">Other (Grad, PhD, etc.)</option>
+              </select>
+            </div>
+            <button className="form__submit" type="submit">Submit</button>
+            <p className="form_subtext">
+              <small>By submitting this form, you'll automatically be added to our mailing list.</small>
+            </p>
+          </form>
         </div>
       </div>
     );
